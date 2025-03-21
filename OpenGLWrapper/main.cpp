5d36269f2a9cd10d4feaa0988f1b81aa70798e8d
Engelhardt Paul <Paul.Engelhardt@candera.eu>
2025-03-21 12:58:57 +0100

    Update main.cpp

--- OpenGLWrapper/main.cpp
+++ OpenGLWrapper/main.cpp
@@ -281,13 +281,13 @@ void loadTexture(std::string texturePath, PixelFormat format, Texture &target, i
         }
         else
         {
-            std::cerr << "Failed to read" << texturePath << std::endl;
+  //          std::cerr << "Failed to read" << texturePath << std::endl;
         }
         file.close();
     }
     else
     {
-        std::cerr << "Failed to open " << texturePath << std::endl;
+//        std::cerr << "Failed to open " << texturePath << std::endl;
     }
 }
 void SetupFunc()
